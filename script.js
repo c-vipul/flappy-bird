7cdbe6172c9373efe416a95f3d1c623cc03d97fd
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-07-15 15:09:45 +0545

    collision detection and score

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const pipeTop = new Image();
 const pipeBottom = new Image();
 const bird = new Image();
 const base = new Image();
+const score = new Image();
+const gameOver = new Image();
 
 backgroundImgs = ['assets/background-day.png', 'assets/background-night.png'];
 birdImgs = ['assets/yellowbird-upflap.png', 'assets/yellowbird-midflap.png', 'assets/yellowbird-downflap.png'];
@@ -14,6 +16,8 @@ background.src = backgroundImgs[Math.floor(Math.random() * backgroundImgs.length
 pipeTop.src = 'assets/pipe-green-top.png';
 pipeBottom.src = 'assets/pipe-green.png';
 base.src = 'assets/base.png';
+score.src = 'assets/0.png';
+gameOver.src = 'assets/gameover.png';
 
 background.onload = () => {
     canvas.width = background.width;
@@ -31,6 +35,8 @@ const gap = 85;
 bird.posX = 10;
 bird.posY = 150;
 gravity = 2;
+score.count = 10;
+gameOver.status = false;
 
 const pipes = [];
 pipes[0] = {
@@ -44,16 +50,31 @@ const drawSprites = () => {
         ctx.drawImage(pipeTop, pipe.posX, pipe.posY);
         ctx.drawImage(pipeBottom, pipe.posX, pipeTop.height + pipe.posY + gap);
         pipe.posX--;
-        if (pipe.posX === 125) {
+        if (pipe.posX === 75) {
             pipes.push({
                 posX: canvas.width,
                 posY: Math.floor(Math.random() * pipeTop.height) - pipeTop.height
             });
         }
+        if (bird.posX + bird.width >= pipe.posX && bird.posX <= pipe.posX + pipeTop.width && (bird.posY <= pipe.posY + pipeTop.height || bird.posY + bird.height >= pipeTop.height + pipe.posY + gap) || bird.height >= background.height - base.height) {
+            gameOver.status = true;
+        }
+        if (pipe.posX === 10) {
+            score.count++;
+        }
     });
 
     ctx.drawImage(base, 0, background.height - base.height);
     ctx.drawImage(bird, bird.posX, bird.posY);
+    // console.log(score.count.toString().split(''));
+    score.count.toString().split('').forEach((num, i) => {
+        console.log(num)
+        score.src = `assets/${num}.png`;
+        ctx.drawImage(score, 120 + (i*25), 50);
+    });
+    if (gameOver.status) {
+        return ctx.drawImage(gameOver, 50, 180);
+    }
 
     bird.posY += gravity;
     window.requestAnimationFrame(drawSprites);
@@ -64,30 +85,3 @@ drawSprites();
 document.addEventListener('keydown', (e) => {
     bird.posY -= 50;
 });
-
-
-
-
-// pipe.onload = () => {
-//     pipe.positionX = canvas.width;
-//     let pipeMotion = () => {
-//         ctx.drawImage(pipe, pipe.positionX, background.height - base.height);
-//         ctx.drawImage(pipe, pipe.positionX, background.height - base.height);
-//     }
-//     pipeMotion();
-// }
-
-
-// base.onload = () => {
-//     base.positionX = 0;
-//     let baseMotion = () => {
-//         ctx.drawImage(base, base.positionX, background.height - base.height);
-//         if (base.positionX === canvas.width-base.width) {
-//             base.positionX = 0;
-//         } else {
-//             base.positionX -= 1;
-//         }
-//         window.requestAnimationFrame(baseMotion);
-//     }
-//     baseMotion();
-// };
